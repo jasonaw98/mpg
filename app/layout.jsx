b168fbdf8564600fceb26f01f1f542a130788db8
jasonaw <jason_aw1998@hotmail.com>
2023-11-17 00:25:37 +0800

    Add background

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-slate-100`}>
         <Navbar />
         <SearchBar />
         <main className='px-9 -z-50 sticky'>
--- app/satisfaction/page.jsx
+++ app/satisfaction/page.jsx
@@ -5,19 +5,8 @@ const Satisfaction = () => {
 
   return (
     <>
-    {/* <div className="flex flex-wrap gap-8 justify-center">
-      {satisfaction.map((item, index) => (
-        <JobCard
-        key={index} // Add a unique key for each rendered JobCard
-        job={item.job}
-        salary={item.salary}
-        des={item.description}
-        />
-        ))}
-    </div> */}
-    <div className="flex justify-around w-full">
-
-    <div className="masonry-grid">
+    <div className="flex justify-center">
+    <div className="columns-2 gap-10">
       {satisfaction.map((item, index) => (
         <JobCard
         key={index} // Add a unique key for each rendered JobCard
